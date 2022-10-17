d3312ea51ee7760dbbba3d9bf7b9aff1f2e62c07
KIM-KYOUNG-OH <kdkg0434@gmail.com>
2022-10-17 13:22:17 +0900

    add .gitignore add application.yml(mysql connection configuration)

---
+++ src/main/java/com/project/payThere/PayThereApplication.java
@@ -0,0 +1,13 @@
+package com.project.payThere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayThereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayThereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/payThere/PayThereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.payThere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayThereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
